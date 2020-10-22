734e18f2dace70b13905f0b33d72c9c61b70b0a2
agadek <andrzej.gadek@grandparade.co.uk>
2020-10-22 22:33:45 +0200

    game

--- src/main/scala/com/chess/Game.scala
+++ src/main/scala/com/chess/Game.scala
@@ -1,23 +1,37 @@
 package com.chess
 
-case class Game(currentPlayer: Player = White, board: Board = Board.build()) {
+import cats.syntax.either._
 
-  def move(from: Address, to: Address): Either[GameError, Game] = ???
+case class Game(currentPlayer: Player = White, board: Board = Board.build()) {
+  def currentPlayerInCheck: Option[BoardError] = board.checkKingStatus(currentPlayer).swap.toOption
+
+  def move(from: Address, to: Address): Either[BoardErrorWrapper, Game] =
+    board.move(from, to)(player = currentPlayer)
+      .leftMap(BoardErrorWrapper)
+      .map(newBoard => this.copy(currentPlayer = currentPlayer.switch, newBoard))
 
 }
 
 
 sealed trait GameError
 
+case class BoardErrorWrapper(boardError: BoardError) extends GameError
+
 
 sealed trait Player {
+  def switch: Player
+
   def isWhite: Boolean
 }
 
 case object White extends Player {
+  def switch: Player = Black
+
   override def isWhite: Boolean = true
 }
 
 case object Black extends Player {
+  def switch: Player = White
+
   override def isWhite: Boolean = false
 }
